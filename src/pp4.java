a2546c0c7dba74345075b592bb789f773f619448
.email <sanjaisanjai6551@gmail.com>
2025-03-26 00:18:11 +0530

    1 2 3 4 5 6 7 8 9 10

--- src/pp4.java
+++ src/pp4.java
@@ -1,7 +1,8 @@
 public class pp4 {
     public static void main(String[] args) {
+        int n=7;
         int num1=1;
-        for (int i = 1; i <5; i++) {
+        for (int i = 1; i <n; i++) {
             for (int j = 1; j <=i; j++) {
                 System.out.print(num1+" ");
                 num1++;
